d8c6075576c8c11ee1d49875f3a8c0f21de716a6
Christopher Kistner <chris.kistner0@gmail.com>
2015-09-16 18:34:18 -0700

    Completed hw04 break_me

---
+++ hw/hw04/break_me.py
@@ -0,0 +1,11 @@
+def exhibit_name_error():
+    name = ['Chris', 'Kevin', 'Tyler']
+    print(title)
+
+def exhibit_attribute_error():
+    import math
+    print (math.pony(4))
+
+
+def exhibit_type_error():
+    print(17 + 'seventeen')
