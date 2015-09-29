62611482c9b7f06358f74e15953ba7a600ea641e
Christopher Kistner <chris.kistner0@gmail.com>
2015-09-29 15:47:12 -0700

    Attempting to decipher the program methods for the Trigram example given in class

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -1,27 +1,22 @@
 import random
 
-import string
 
 file = open('Normandy_exert.txt', 'r')
 
 lines = file.readlines()
 words = []
 trigrams = {}
+begining = "How shall"
 
 for line in lines:
     words += line.split(' ')
 
-for i, word in enumerate(words):
-    if (i < len(words) - 2):
-        trigrams[(word, words[i + 1])] = words[i + 2]
+for i in (range(len(words) - 2)):
+    phrase = words[i] + " " + words[i + 1]
+    if (phrase in trigrams):
+        trigrams[phrase].append(words[i + 2])
+    else:
+        trigrams[phrase] = [words[i + 2]]
 
 
-print(str(words))
-print(str(trigrams))
-
-'''for i in (range(len(words) - 2)):
-   phrase = words[i] + " " + words[i + 1]
-   if (phrase in trigrams):
-       trigrams[phrase].append(words[i + 2])
-   else:
-       trigrams[phrase] = [words[i + 2]]'''
+file.close()
