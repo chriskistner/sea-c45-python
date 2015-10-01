4bb634c4f652e7df17bfbb5fcb77f81ba1298ba3
Christopher Kistner <chris.kistner0@gmail.com>
2015-10-01 15:25:06 -0700

    started hw18

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -7,5 +7,18 @@ Python class example.
 
 # The start of it all:
 # Fill it all in here.
+
+
 class Element(object):
-    pass
+
+
+    def __init__(self, name=''):
+        self.name = name
+        self.children = []
+
+    def append(self, child):
+        self.children.append(child)
+
+    def render(self, f):
+        f.write("<{name}>".format(name=self.name))
+        f.write("</{name}>").format(name=self.name
