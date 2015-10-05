32a9d5ced9806554a0917822301ba210d69844f0
Christopher Kistner <chris.kistner0@gmail.com>
2015-10-05 16:52:27 -0700

    Got hw18 running through #3, struggling with syntax on 4

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -11,34 +11,47 @@ Python class example.
 
 class Element(object):
 
+
     def __init__(self, content='', name=''):
         self.name = name
         self.content = content
         self.children = []
+        self.style = []
 
-    def append(self, child):
+
+    def append(self, child, style):
         self.children.append(child)
+        self.style.append(style)
+
 
     def render(self, f):
-        f.write("\n <{name}>".format(name=self.name))
-        if self.children == []:
-            f.write(self.content)
-        else:
-            for items in self.children:
-                items.render(f)
-        f.write("\n </{name}>".format(name=self.name))
-
+        f.write(" \n <{name} {style}>".format(name=self.name, style=self.style)
+            if self.children == []:
+                f.write(self.content)
+            else
+                for items in self.children:
+                    items.render(f)
+                    f.write("\n </{name}>".format(name=self.name))
 
 class Html(Element):
-    def __init__(self, content=''):
-        Element.__init__(self, content='', name='html')
-
+    def __init__(self, content='', style=''):
+        Element.__init__(self, content='', name='html', style=style)
 
 class Body(Element):
-    def __init__(self, content=''):
-        Element.__init__(self, content='', name='body')
+    def __init__(self, content='', style=''):
+        Element.__init__(self, content='', name='body', style=style)
 
 
 class P(Element):
-    def __init__(self, content=''):
-        Element.__init__(self, content=content, name='p')
+    def __init__(self, content='', style=''):
+        Element.__init__(self, content=content, name='p', style=style)
+
+
+class Head(Element):
+    def __init__(self, content='', style=''):
+        Element.__init__(self, content=content, name='head', style=style)
+
+
+class Title(Element):
+    def __init__(self, content='', style=''):
+        Element.__init__(self, content=content, name='title', style=style)
