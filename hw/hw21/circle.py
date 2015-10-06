46ec8f60a18d83ce63e769366cf8de64438799bd
Christopher Kistner <chris.kistner0@gmail.com>
2015-10-05 21:04:42 -0700

    Made some property value adjustments to the file

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -12,3 +12,17 @@ class Circle(object):
         self.radius = radius
         self.diameter = 2 * radius
         self.area = math.pi * radius ** 2
+
+    def _get_d(self):
+        return self._diamter
+
+    def _set_d(self, value):
+        self.diamater = value
+        self.radius = value / 2
+        self.area = math.pi * self.radius ** 2
+
+    def _del_d(self):
+        del self._diameter
+
+    diamater = property(_get_d, _set_d, _del_d)
+    doc = "largest distance between any two points on a circle"
