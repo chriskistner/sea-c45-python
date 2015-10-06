4dfaa0e3792980b8e14da0f8229a506fbe0b3ec7
Christopher Kistner <chris.kistner0@gmail.com>
2015-10-05 19:01:17 -0700

    Up to output 6 on hw18

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -11,47 +11,63 @@ Python class example.
 
 class Element(object):
 
-
-    def __init__(self, content='', name=''):
+    def __init__(self, content='', name='', style='', link=''):
         self.name = name
         self.content = content
         self.children = []
-        self.style = []
+        self.style = style
+        self.link = link
 
-
-    def append(self, child, style):
+    def append(self, child):
         self.children.append(child)
-        self.style.append(style)
-
 
     def render(self, f):
-        f.write(" \n <{name} {style}>".format(name=self.name, style=self.style)
-            if self.children == []:
-                f.write(self.content)
-            else
-                for items in self.children:
-                    items.render(f)
-                    f.write("\n </{name}>".format(name=self.name))
+        if self.style == '' and self.link == '':
+            f.write(" \n     <{name}>".format(name=self.name))
+        elif self.style == '' and self.link != '':
+            f.write(" \n     <{name} href= '"'{link}'"'>".format
+                    (name=self.name, style=self.link))
+        else:
+            f.write(" \n     <{name} style= '"'{style}'"'' href= '"'{link}'"'>"
+                    .format(name=self.name, style=self.style, link=self.link))
+        if self.children == []:
+            f.write(self.content)
+        else:
+            for items in self.children:
+                items.render(f)
+                f.write("\n</{name}>".format(name=self.name))
+
 
 class Html(Element):
     def __init__(self, content='', style=''):
-        Element.__init__(self, content='', name='html', style=style)
+        Element.__init__(self, content='', style=style, name='html')
+
 
 class Body(Element):
     def __init__(self, content='', style=''):
-        Element.__init__(self, content='', name='body', style=style)
+        Element.__init__(self, content='', style=style, name='body')
 
 
 class P(Element):
     def __init__(self, content='', style=''):
-        Element.__init__(self, content=content, name='p', style=style)
+        Element.__init__(self, content=content, style=style, name='p')
 
 
 class Head(Element):
     def __init__(self, content='', style=''):
-        Element.__init__(self, content=content, name='head', style=style)
+        Element.__init__(self, content=content, style=style, name='head')
 
 
 class Title(Element):
     def __init__(self, content='', style=''):
-        Element.__init__(self, content=content, name='title', style=style)
+        Element.__init__(self, content=content, style=style, name='title')
+
+
+class Hr(Element):
+    def __init__(self, content='', style=''):
+        Element.__init__(self, content=content, style=style, name='title')
+
+
+class A(Element):
+    def __init__(self, link='', content='', style=''):
+        Element.__init__(self, content=content, link=link, style='', name='A')
--- hw/hw18/run_html_render.py
+++ hw/hw18/run_html_render.py
@@ -78,7 +78,7 @@ render(page, u"test_html_output3.html")
 '''
 # # Step 4
 # ##########
-
+'''
 page = hr.Html()
 
 head = hr.Head()
@@ -94,52 +94,52 @@ body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, b
 page.append(body)
 
 render(page, u"test_html_output4.html")
-
+'''
 # # Step 5
 # #########
+'''
+page = hr.Html()
 
-# page = hr.Html()
+head = hr.Head()
+head.append(hr.Title(u"PythonClass = Revision 1087:"))
 
-# head = hr.Head()
-# head.append(hr.Title(u"PythonClass = Revision 1087:"))
+page.append(head)
 
-# page.append(head)
+body = hr.Body()
 
-# body = hr.Body()
+body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
+              style=u"text-align: center; font-style: oblique;"))
 
-# body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
-#               style=u"text-align: center; font-style: oblique;"))
+body.append(hr.Hr())
 
-# body.append(hr.Hr())
+page.append(body)
 
-# page.append(body)
-
-# render(page, u"test_html_output5.html")
-
+render(page, u"test_html_output5.html")
+'''
 # # Step 6
 # #########
 
-# page = hr.Html()
+page = hr.Html()
 
-# head = hr.Head()
-# head.append(hr.Title(u"PythonClass = Revision 1087:"))
+head = hr.Head()
+head.append(hr.Title(u"PythonClass = Revision 1087:"))
 
-# page.append(head)
+page.append(head)
 
-# body = hr.Body()
+body = hr.Body()
 
-# body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
-#               style=u"text-align: center; font-style: oblique;"))
+body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
+               style=u"text-align: center; font-style: oblique;"))
 
-# body.append(hr.Hr())
+body.append(hr.Hr())
 
-# body.append(u"And this is a ")
-# body.append( hr.A(u"http://google.com", "link") )
-# body.append(u"to google")
+body.append(u"And this is a ")
+body.append( hr.A(u"http://google.com", "link") )
+body.append(u"to google")
 
-# page.append(body)
+page.append(body)
 
-# render(page, u"test_html_output6.html")
+render(page, u"test_html_output6.html")
 
 # # Step 7
 # #########
