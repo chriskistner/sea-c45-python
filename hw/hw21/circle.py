e3d5b0eda676bc0b7b6bdb88c309c963cf92912c
Christopher Kistner <chris.kistner0@gmail.com>
2015-10-05 20:16:22 -0700

    Started Circle Testing Work

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -7,4 +7,8 @@ import math
 
 
 class Circle(object):
-    pass
+
+    def __init__(self, radius):
+        self.radius = radius
+        self.diameter = 2 * radius
+        self.area = math.pi * radius ** 2
