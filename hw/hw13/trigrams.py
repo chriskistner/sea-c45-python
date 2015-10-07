1a12e86a5e35a95604e42318822b7e5b980f62f3
Christopher Kistner <chris.kistner0@gmail.com>
2015-10-06 22:03:32 -0700

    Tried to work off what others showed me but still can't get this too run properly.

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -1,15 +1,15 @@
 import random
 
 
-file = open('Normandy_exert.txt', 'r')
+file = open("Normandy_exert.txt", 'r')
 
 lines = file.readlines()
 words = []
 trigrams = {}
-begining = "How shall"
+start_phrase = "We must"
 
 for line in lines:
-    words += line.split(' ')
+    words += line.split()
 
 for i in (range(len(words) - 2)):
     phrase = words[i] + " " + words[i + 1]
@@ -18,5 +18,28 @@ for i in (range(len(words) - 2)):
     else:
         trigrams[phrase] = [words[i + 2]]
 
+done = False
+
+word_2 = start_phrase.split()[0]
+word_1 = start_phrase.split()[1]
+sentance = start_phrase
+story = 0
+
+while not done:
+    story = story + 1
+    phrase = word_2 + " " + word_1
+    if (phrase in trigrams):
+        numb_of_words = len(trigrams[phrase]) - 1
+        i = random.randint(0, numb_of_words)
+        list_words = trigrams[phrase]
+        new_word = list_words[i]
+        sentance = sentance + " " + new_word
+        word_2 = word_1
+        word_1 = new_word
+
+    else:
+        done = True
+
+print(sentance)
 
 file.close()
