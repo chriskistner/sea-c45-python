f7a1a8911433519f644d293103924928cf1c4de3
Christopher Kistner <chris.kistner0@gmail.com>
2015-10-07 20:19:48 -0700

    Completed the circle testing methods

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -10,19 +10,48 @@ class Circle(object):
 
     def __init__(self, radius):
         self.radius = radius
-        self.diameter = 2 * radius
-        self.area = math.pi * radius ** 2
+        self._diameter = 2 * radius
+        self._area = math.pi * radius ** 2
 
     def _get_d(self):
-        return self._diamter
+        return self._diameter
 
     def _set_d(self, value):
-        self.diamater = value
+        self._diameter = value
         self.radius = value / 2
-        self.area = math.pi * self.radius ** 2
+        self._area = math.pi * self.radius ** 2
 
     def _del_d(self):
         del self._diameter
 
-    diamater = property(_get_d, _set_d, _del_d)
+    diameter = property(_get_d, _set_d, _del_d)
+
+    def _get_a(self):
+        return self._area
+    area = property(_get_a)
+
+    def __str__(self):
+        return "Circle with radius: {:.6f}".format(self.radius)
+
+    def __repr__(self):
+        return "Circle({r})".format(r=self.radius)
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other)
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __le__(self, other):
+        return self.radius <= other.radius
+
+    def __gt__(self, other):
+        return self.radius > other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
     doc = "largest distance between any two points on a circle"
